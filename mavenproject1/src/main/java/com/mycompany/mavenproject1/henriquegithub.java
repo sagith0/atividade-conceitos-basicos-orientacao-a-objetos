42fe485b015a3af5b4dc10f9d52586c8c37c7187
unknown <henriquerodrigoricardo@gmail.com>
2025-03-27 15:04:03 -0300

    inicializou o projeto

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/henriquegithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author aluno
+ */
+public class henriquegithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
